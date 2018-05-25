530b45fb4c59de2440473f714b553e20fda260dc
Mikael <MikaelAraya12@gmail.com>
2018-05-25 17:31:00 +0300

    added student managment function

--- application/controllers/api/Students.php
+++ application/controllers/api/Students.php
@@ -15,7 +15,7 @@ class Students extends API
         $this->load->model('students_model');
     }
 
-    function index_get()
+    function index_get($id = NULL)
     {
         $result['result'] = $this->students_model->get_students();
         if(count($result)>0)
@@ -25,6 +25,51 @@ class Students extends API
             $result['columns']=array_keys((array)$first_record);
         }
         $this->response($result,API::HTTP_OK);
+    
+    }
+
+    function index_post() {
+
+        $this->load->library("form_validation");
+  
+        $this->form_validation->set_rules("full_name", "Full Name", "required");
+        $this->form_validation->set_rules("gender", "Gender", "required");
+            $result['success'] = false;
+          if($this->form_validation->run() === FALSE) {
+                $this->response($result, API::HTTP_OK);
+          } else {
+              $data = array(
+                'full_name' => $this->input->post('full_name'),
+                'gender' => $this->input->post('gender'),
+                'blood_group' => $this->input->post('blood_group'),
+                'birthdate' => $this->input->post('birthdate')
+                
+            );
+            $result['success'] = ($this->students_model->save_student($data)) ? true : false;
+            $this->response($result, API::HTTP_OK);
+          }
+
+        
+
+    }
+
+    function index_put($id) {
+        $this->input->raw_input_stream;
+            $data = array(
+                'full_name' => $this->input->input_stream('full_name'),
+                'gender' => $this->input->input_stream('gender'),
+                'birthdate' => $this->input->input_stream('birthdate'),
+                'blood_group' => $this->input->input_stream('blood_group')
+            );
+           $result['success'] = $this->students_model->update_student($id, $data) ? true : false;
+           $this->response($result, API::HTTP_OK);
+
+    }
+
+    
+    function index_delete($id) {
+        $result['sucess'] = ($this->students_model->delete_student($id)) ? true: false;
+        $this->response($result, API::HTTP_OK);
     }
 
 
--- application/models/Students_model.php
+++ application/models/Students_model.php
@@ -6,25 +6,36 @@
  * Time: 8:18 AM
  */
 
-class Students_model extends MY_Model
-{
+class Students_model extends MY_Model {
     function __construct() {
       parent::__construct();
       $this->load->database();
     }
-    function lists()
-    {
-        $table=[
-            'from'=>'students',
-            'select'=>['id','full_name','id_no','birthdate','gender','blood_group']
-        ];
-        return $this->select($table);
-    }
 
-    function get_students() {
+    function get_students($id = NULL) {
+        if(!is_null($id)) {
+        }
       $result = $this->db->get("students");
 
       return $result->result_array();
+    }
+
+    function save_student($new_student) {
+        $this->db->insert('students', $new_student);
+        return ($this->db->affected_rows() > 0) ? true : false; 
 
     }
+
+    function update_student($id, $data) {
+        $this->db->where('id', $id);
+       return $this->db->update('students', $data);
+        
+    }
+
+    function delete_student($id) {
+         $this->db->delete('students', array('id' => $id));
+        return ($this->db->affected_rows() > 0) ? true : false;
+    }
+
+
 }
