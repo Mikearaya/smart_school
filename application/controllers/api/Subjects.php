2e0383592793ac66b981bee427cab3458bfc45ea
Mikael Araya <Mikaelaraya@example.com>
2018-06-04 22:32:16 +0300

    added the basic subject API features

--- application/controllers/api/Subjects.php
+++ application/controllers/api/Subjects.php
@@ -14,9 +14,9 @@ class Subjects extends API
         $this->load->model('subjects_model');
     }
 
-    function index_get()
+    function index_get($id = NULL)
     {
-        $result["result"]= $this->subjects_model->get_subject();
+        $result["result"]= $this->subjects_model->get_subject($id);
         if(count($result)>0)
         {
             $first_record=$result['result'][0];
@@ -24,6 +24,26 @@ class Subjects extends API
             $result['columns']=array_keys((array)$first_record);
         }
         $this->response($result,API::HTTP_OK);
+    }
+    function index_post($id = NULL) {
+        $result['success'] = false;
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('title', 'Subject Name', 'required');
+        $this->form_validation->set_rules('subject_type', 'Subject type', 'required');
+        $this->form_validation->set_rules('grade_weightage', 'Subject Credit Hr.', 'required');
+
+        if ($this->form_validation->run() === false ) {
+            $this->response($this->validation_errors(), API::HTTP_OK);
+        } else {
+            $success = $this->subjects_model->save_subjects($this->input->post(), $id);
+            if($success) {
+                $result['success'] = true;
+                $result['subjectId'] = $success;
+            }
+            $this->response($result, API::HTTP_OK);
+        }
+
+        
     }
 
 }
--- application/models/Subjects_model.php
+++ application/models/Subjects_model.php
@@ -19,9 +19,51 @@ class Subjects_model extends MY_Model
         return $this->select($table);
     }
 
-    function get_subject() {
-
-      $result = $this->db->get("courses");
-      return $result->result_array();
+    function get_subject($id = NULL) {
+        if (!is_null($id)) {
+            $this->db->where('id', $id);
+            $result = $this->db->get('subjects');
+            return $result->row_array();
+        } else {
+            $result = $this->db->get("subjects");
+            return $result->result_array();
+        }
+      
     }
+
+    function save_subjects( $input, $id = NULL ) {
+            $data = $this->initialize_data_model($input);
+            $last_id = NULL;
+            $this->db->trans_begin();
+                try {
+            if($id) {
+                $this->update_subjects($data, $id);
+                $last_id = $id;
+            } else {
+                $this->db->insert('subjects', $data);
+                $last_id = $this->db->insert_id();
+            }
+            } catch(Exception $e) {
+                echo $e->getMessage();
+            }
+            $this->db->trans_complete();
+
+        return ($this->db->trans_status() === FALSE) ? false : $last_id;
+    }
+
+    function update_subjects($input, $id) {
+        $data = $this->initialize_data_model($input);
+        $this->db->where('id', $id);
+        $this->db->update('subjects', $data);
+    }
+
+    private function initialize_data_model($input_data) {
+        return array(
+            "title" => $input_data['title'],
+            "subject_type" => $input_data['subject_type'],
+            "grade_weightage" => $input_data['grade_weightage']
+        );
+    }
+
 }
+
